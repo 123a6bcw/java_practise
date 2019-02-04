7ea4c6baff814d7a508e54877ed0e8906eb61cd4
123a6bcw <ga.karandash@yandex.ru>
2019-02-04 03:12:21 +0300

    Added private Node class with initial functionality

--- trie/src/main/java/ru/hse/hw3/trie/Trie.java
+++ trie/src/main/java/ru/hse/hw3/trie/Trie.java
@@ -1,4 +1,41 @@
 package ru.hse.hw3.trie;
 
+import java.util.HashMap;
+
 public class Trie {
+
+    /**
+     * Class representing the node inside trie.
+     */
+    private class Node {
+        /**
+         * By char gives Node representing corresponding child string for this node.
+         */
+        private HashMap<Character, Node> sonNodes;
+
+        private Node() {
+            sonNodes = new HashMap<>();
+        }
+
+        /**
+         * By char gives Node representing corresponding child string for this node.
+         */
+        private Node getSonNode(char key) {
+            return sonNodes.get(key);
+        }
+
+        /**
+         * Creates new empty Node with given key, throws exception if Node with given key already exists.
+         */
+        private Node createSonNode(char key) {
+            /*
+            Should I throw IllegalArgumentExceptions etc in the non-public interface or I may avoid it for optimisation purposes?
+             */
+            if (sonNodes.containsKey(key)) {
+                throw new IllegalArgumentException("Can't create son node cause node with given key already exists");
+            }
+
+            return sonNodes.put(key, new Node());
+        }
+    }
 }
