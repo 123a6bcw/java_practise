e14aa58b28cf608b30dd224d2f7f9ee23763ce36
123a6bcw <ga.karandash@yandex.ru>
2019-02-04 04:51:35 +0300

    Extended findDeepestExistingNode functionality to support changing sizes of nodes. Added "add" method.

--- trie/src/main/java/ru/hse/hw3/trie/Trie.java
+++ trie/src/main/java/ru/hse/hw3/trie/Trie.java
@@ -26,21 +26,44 @@ public class Trie {
 
     /**
      * Tries to find the longest prefix of element that exists in trie (not necessary as terminal string)
-     * Returns a pair (packed in special class) of found Node and corresponding element's prefix
+     *
+     * If changeSize is 0, does not change sizes of nodes, changeSize is 1, increase it by 1 (using to add new string to trie),
+     * if changeSize if -1, decrease it by 1 (using to remove terminal string from trie after making sure this terminal string exists), in any other case throws exception
+     *
+     * Returns a pair (packed in special class) of found Node and corresponding element's prefix.
      */
     @NotNull
-    private ParsedNode findDeepestExistingNode(@NotNull String element) {
+    private ParsedNode findDeepestExistingNode(@NotNull String element, int changeSize) {
+        if (changeSize != -1 && changeSize != 0 && changeSize != 1) {
+            throw new IllegalArgumentException("changeSize should be either -1, 0 or 1");
+        }
+
         Node currentNode = root;
         for (int i = 0; i < element.length(); i++) {
+            if (changeSize == 1) {
+                currentNode.increaseSize();
+            } else
+            if (changeSize == -1) {
+                currentNode.decreaseSize();
+            }
+
             Node sonNode = currentNode.getSonNode(element.charAt(i));
             if (sonNode == null) {
                 return new ParsedNode(currentNode, i);
             } else {
                 currentNode = sonNode;
+
+                if (changeSize == -1 && sonNode.getSize() == 1) {
+                    /*
+                    Son node represents only one terminal string and it's going to be deleted, so we erasing memory by deleting reference to it
+                    Since we always deletes son nodes, root of the trie will never be deleted
+                    */
+                    currentNode.removeSonNode(element.charAt(i));
+                }
             }
         }
 
-        return new ParsedNode(currentNode, element.length());
+        return new ParsedNode(currentNode, element.length()); //Trie contains entire given string (as prefix of terminal).
     }
 
     /**
@@ -66,6 +89,21 @@ public class Trie {
     }
 
     /**
+     * Adds terminal string to the trie
+     * Returns true if Trie already contained given terminal string
+     */
+    public boolean add(@NotNull String element) {
+        ParsedNode parsedNode = findDeepestExistingNode(element, 1);
+        Node currentNode = parsedNode.getParsedNode();
+        for (int i = parsedNode.getParsedPrefix(); i < element.length(); i++) {
+            currentNode = currentNode.createSonNode(element.charAt(i));
+        }
+
+        currentNode.increaseTerminality();
+        return currentNode.getTerminality() > 1;
+    }
+
+    /**
      * Class representing the node inside trie.
      */
     private class Node {
@@ -86,7 +124,7 @@ public class Trie {
 
         private Node() {
             sonNodes = new HashMap<>();
-            size = 0; //Still prefer to do it manually (just in case)
+            size = 1; //Nodes should always be creating and existing to represent some stored string
             terminalSize = 0;
         }
 
@@ -102,7 +140,7 @@ public class Trie {
          * Creates new empty Node with given key, throws exception if Node with given key already exists.
          */
         @NotNull
-        private Node createSonNode(char key, boolean isTerminal) {
+        private Node createSonNode(char key) {
             /*
             Should I throw IllegalArgumentExceptions etc in the non-public interface or I may avoid it for optimisation purposes?
              */
@@ -115,11 +153,49 @@ public class Trie {
         }
 
         /**
-         * Make one more terminal String end in this Node, which means increasing size and terminalSize by 1.
+         * Removes son node with given key from sons of this node. Throws exception if there is no son node with given key
+         */
+        private void removeSonNode(char key) {
+            if (!sonNodes.containsKey(key)) {
+                throw new IllegalArgumentException("Can't remove son node with given key cause it does not exist");
+            }
+
+            sonNodes.remove(key);
+        }
+
+        /**
+         * Makes one more terminal String end in this Node, which means increasing terminalSize by 1.
          */
         private void increaseTerminality() {
-            size++;
             terminalSize++;
+        }
+
+        /**
+         * Makes one less terminal String end in this Node, which means decreasing terminalSize by 1.
+         */
+        private void decreaseTerminality() {
+            terminalSize--;
+        }
+
+        /**
+         * Marks that one more terminal string ends in this subtree
+         */
+        private void increaseSize() {
+            size++;
+        }
+
+        /**
+         * Marks that string was deleted from this subtree
+         */
+        private void decreaseSize() {
+            size--;
+        }
+
+        /**
+         * Returns number of terminal string in this subtree
+         */
+        private int getSize() {
+            return size;
         }
 
         /**
@@ -134,6 +210,13 @@ public class Trie {
          */
         private  boolean isTerminal() {
             return terminalSize > 0;
+        }
+
+        /**
+         * Returns number of terminal strings ended in this Node
+         */
+        private int getTerminality() {
+            return terminalSize;
         }
     }
 }
