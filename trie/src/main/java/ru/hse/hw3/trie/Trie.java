6a41a82ac335b50cc4b9d13c87bd8764305e8be6
123a6bcw <ga.karandash@yandex.ru>
2019-02-04 03:22:59 +0300

    Extends Node functionality with tracking size of terminal strings

--- trie/src/main/java/ru/hse/hw3/trie/Trie.java
+++ trie/src/main/java/ru/hse/hw3/trie/Trie.java
@@ -3,6 +3,16 @@ package ru.hse.hw3.trie;
 import java.util.HashMap;
 
 public class Trie {
+    /**
+     * root of the trie
+     */
+    private Node root;
+
+    public Trie() {
+        root = new Node();
+    }
+
+
 
     /**
      * Class representing the node inside trie.
@@ -13,8 +23,20 @@ public class Trie {
          */
         private HashMap<Character, Node> sonNodes;
 
+        /**
+         * How many terminal strings ended somewhere in subtree of this Node
+         */
+        private int size;
+
+        /**
+         * How many terminal strings ended exactly in this Node
+         */
+        private int terminalSize;
+
         private Node() {
             sonNodes = new HashMap<>();
+            size = 0; //Still prefer to do it manually (just in case)
+            terminalSize = 0;
         }
 
         /**
@@ -27,7 +49,7 @@ public class Trie {
         /**
          * Creates new empty Node with given key, throws exception if Node with given key already exists.
          */
-        private Node createSonNode(char key) {
+        private Node createSonNode(char key, boolean isTerminal) {
             /*
             Should I throw IllegalArgumentExceptions etc in the non-public interface or I may avoid it for optimisation purposes?
              */
@@ -36,6 +58,14 @@ public class Trie {
             }
 
             return sonNodes.put(key, new Node());
+        }
+
+        /**
+         * Make one more terminal String end in this Node, which means increasing size and terminalSize by 1.
+         */
+        private void increaseTerminality() {
+            size++;
+            terminalSize++;
         }
     }
 }
