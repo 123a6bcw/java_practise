d591dcf27b1b832a5141cf3be8ffe1cfd5cb1824
123a6bcw <ga.karandash@yandex.ru>
2019-02-06 03:15:01 +0300

    Added test class skeleton

---
+++ UnbalancedTreeSet/src/test/java/ru/hse/hw3/unbalancedtreeset/UnbalancedTreeSetTest.java
@@ -0,0 +1,81 @@
+package ru.hse.hw3.unbalancedtreeset;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UnbalancedTreeSetTest {
+    UnbalancedTreeSet<Integer> mySet;
+
+    @BeforeEach
+    private void initialise() {
+        mySet = new UnbalancedTreeSet<>(Comparator.naturalOrder());
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void iterator() {
+    }
+
+    @Test
+    void descendingIterator() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void descendingSet() {
+    }
+
+    @Test
+    void first() {
+    }
+
+    @Test
+    void last() {
+    }
+
+    @Test
+    void lower() {
+    }
+
+    @Test
+    void floor() {
+    }
+
+    @Test
+    void ceiling() {
+    }
+
+    @Test
+    void higher() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void retainAll() {
+    }
+}
