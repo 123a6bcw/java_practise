d0804274b0cccb69c6737f8f9ac54a0035cded27
123a6bcw <ga.karandash@yandex.ru>
2019-02-26 05:44:40 +0300

    Added class to store in database

---
+++ DataBase/src/main/java/ru/hse/hw4/database/DataRecord.java
@@ -0,0 +1,30 @@
+package ru.hse.hw4.database;
+
+import org.mongodb.morphia.annotations.*;
+
+/**
+ *
+ */
+@Entity
+public class DataRecord {
+        private String name;
+        private String phone;
+
+        public DataRecord() {
+            name = null;
+            phone = null;
+        }
+
+        public DataRecord(String name, String record) {
+            this.name = name;
+            this.phone = record;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+}
