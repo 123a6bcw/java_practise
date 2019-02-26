866e1eb55c2bfee3f962ebb7a511cccd3b20390c
123a6bcw <ga.karandash@yandex.ru>
2019-02-26 08:59:35 +0300

    Now closing MongoClient() -> exit starts to work. Some user messages updates

--- DataBase/src/main/java/ru/hse/hw4/database/PhonebookDataBase.java
+++ DataBase/src/main/java/ru/hse/hw4/database/PhonebookDataBase.java
@@ -12,6 +12,7 @@ import ch.qos.logback.classic.LoggerContext;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Scanner;
 
 /**
@@ -23,7 +24,8 @@ public class PhonebookDataBase {
      *
      */
     public static void main(String[] argc) {
-        ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger("org.mongodb.driver").setLevel(Level.OFF); //TODO
+        //Hides mongoDB logs from console (too many trash).
+        ((LoggerContext) LoggerFactory.getILoggerFactory()).getLogger("org.mongodb.driver").setLevel(Level.ERROR);
 
         String dataBaseName = null;
         if (argc.length != 0) {
@@ -32,55 +34,60 @@ public class PhonebookDataBase {
             dataBaseName = "mainDataBase";
         }
 
-        var inputScanner = new Scanner(System.in);
-        final var morphia = new Morphia();
-        morphia.mapPackage("ru.hse.hw4.database");
-        final Datastore datastore = morphia.createDatastore(new MongoClient(), dataBaseName);
+        //Google says I'm not suppose to explicitly close MongoClient, but does not work otherwise.
+        try (var inputScanner = new Scanner(System.in);
+             var mongoClient = new MongoClient()) {
 
-        System.out.println("Write help to get help");
+            final var morphia = new Morphia();
+            morphia.mapPackage("ru.hse.hw4.database");
 
-        boolean stopInteraction = false; //
-        while (!stopInteraction) {
-            if (inputScanner.hasNextLine()) {
-                var commandScanner = new Scanner(inputScanner.nextLine());
-                switch (commandScanner.next()) {
-                    case ("help"):
-                        printHelp();
-                        break;
-                    case ("exit"):
-                        stopInteraction = true;
-                        break;
-                    case ("addRecord"):
-                        addRecord(commandScanner, datastore);
-                        break;
-                    case ("findPhones"):
-                        findPhones(commandScanner, datastore);
-                        break;
-                    case ("findNames"):
-                        findNames(commandScanner, datastore);
-                        break;
-                    case ("deleteRecord"):
-                        deleteRecord(commandScanner, datastore);
-                        break;
-                    case ("changeName"):
-                        changeName(commandScanner, datastore);
-                        break;
-                    case("changePhone"):
-                        changePhone(commandScanner, datastore);
-                        break;
-                    case ("printAll"):
-                        printAll(datastore);
-                        break;
+            final Datastore datastore = morphia.createDatastore(mongoClient, dataBaseName);
+
+            System.out.println("\nWrite help to get help");
+
+            boolean stopInteraction = false; //
+            while (!stopInteraction) {
+                if (inputScanner.hasNextLine()) {
+                    var commandScanner = new Scanner(inputScanner.nextLine());
+                    switch (commandScanner.next()) {
+                        case ("help"):
+                            printHelp();
+                            break;
+                        case ("exit"):
+                            stopInteraction = true;
+                            break;
+                        case ("addRecord"):
+                            addRecord(commandScanner, datastore);
+                            break;
+                        case ("findPhones"):
+                            findPhones(commandScanner, datastore);
+                            break;
+                        case ("findNames"):
+                            findNames(commandScanner, datastore);
+                            break;
+                        case ("deleteRecord"):
+                            deleteRecord(commandScanner, datastore);
+                            break;
+                        case ("changeName"):
+                            changeName(commandScanner, datastore);
+                            break;
+                        case ("changePhone"):
+                            changePhone(commandScanner, datastore);
+                            break;
+                        case ("printAll"):
+                            printAll(datastore);
+                            break;
+                    }
                 }
             }
         }
     }
 
     /**
-     *
+     *Error:(10, 30) java: package ch.qos.logback.classic does not exist
      */
     private static void printHelp() {
-        System.out.println("Please use one of the following commands:\n" +
+        System.out.println("\nPlease use one of the following commands:\n" +
                 "exit                                          exit the program\n" +
                 "addRecord name phoneNumber                    adds new record with given name and phone number\n" +
                 "findPhones {-byName name}                     finds all phones by given parameter\n" +
@@ -88,7 +95,7 @@ public class PhonebookDataBase {
                 "deleteRecord name phoneNumber                 deletes given record from the base\n" +
                 "changeName name phoneNumber newName           changes name to newName for given record\n" +
                 "changePhone name phoneNumber newPhoneNumber   changes phoneNumber to newPhoneNumber for given record\n" +
-                "printAll                                      prints all record in the base");
+                "printAll                                      prints all record in the base\n");
     }
 
     /**
@@ -97,13 +104,14 @@ public class PhonebookDataBase {
     private static void addRecord(Scanner commandScanner, Datastore datastore) {
         String[] parameters = getParameters(commandScanner, 2);
         if (parameters == null) {
-            System.out.println("name or phone number is missing. No record has been added.");
+            System.out.println("name or phone number is missing. No record has been added.\n");
             return;
         }
 
         String name = parameters[0], phone = parameters[1];
 
         datastore.save(new DataRecord(name, phone));
+        System.out.println("Ok! Record " + name + " " + phone + " has been added\n");
     }
 
     /**
@@ -112,7 +120,7 @@ public class PhonebookDataBase {
     private static void findPhones(Scanner commandScanner, Datastore datastore) {
         String key = getParameter(commandScanner);
         if (key == null) {
-            System.out.println("No key specified for search");
+            System.out.println("No key specified for search\n");
             return;
         }
 
@@ -120,22 +128,26 @@ public class PhonebookDataBase {
             case ("-byName"):
                 String name = getParameter(commandScanner);
                 if (name == null) {
-                    System.out.println("no name specified for -byName search");
+                    System.out.println("no name specified for -byName search\n");
                     break;
                 }
 
                 List<DataRecord> records = datastore.find(DataRecord.class).field("name").equal(name).asList();
                 if (records.size() == 0) {
-                    System.out.println("no records with given name");
+                    System.out.println("no records with given name\n");
                 } else {
-                    for (var record : records) {
-                        System.out.print(record.getPhone() + " ");
+                    for (var iterator = records.iterator(); iterator.hasNext();) {
+                        var record = iterator.next();
+                        System.out.print(record.getPhone());
+                        if (iterator.hasNext()) {
+                            System.out.println(", ");
+                        }
                     }
-                    System.out.println();
+                    System.out.print("\n\n");
                 }
                 break;
             default:
-                System.out.println("unknown key");
+                System.out.println("unknown key\n");
         }
     }
 
@@ -145,7 +157,7 @@ public class PhonebookDataBase {
     private static void findNames(Scanner commandScanner, Datastore datastore) {
         String findNamesKey = getParameter(commandScanner);
         if (findNamesKey == null) {
-            System.out.println("key is missing");
+            System.out.println("key is missing\n");
             return;
         }
 
@@ -153,21 +165,25 @@ public class PhonebookDataBase {
             case ("-byPhone"):
                 String phone = getParameter(commandScanner);
                 if (phone == null) {
-                    System.out.println("no phone number specified for -byPhone search");
+                    System.out.println("no phone number specified for -byPhone search\n");
                 }
 
                 List<DataRecord> records = datastore.find(DataRecord.class).field("phone").equal(phone).asList();
                 if (records.size() == 0) {
-                    System.out.println("no records with given phone");
+                    System.out.println("no records with given phone\n");
                 } else {
-                    for (var record : records) {
-                        System.out.print(record.getName() + " ");
+                    for (var iterator = records.iterator(); iterator.hasNext();) {
+                        var record = iterator.next();
+                        System.out.print(record.getName());
+                        if (iterator.hasNext()) {
+                            System.out.print(", ");
+                        }
                     }
-                    System.out.println();
+                    System.out.print("\n\n");
                 }
                 break;
             default:
-                System.out.println("unknown key");
+                System.out.println("unknown key\n");
         }
     }
 
@@ -177,13 +193,14 @@ public class PhonebookDataBase {
     private static void deleteRecord(Scanner commandScanner, Datastore datastore) {
         String[] parameters = getParameters(commandScanner, 3);
         if (parameters == null) {
-            System.out.println("name or phone not specified, no record has been deleted");
+            System.out.println("name or phone not specified, no record has been deleted\n");
             return;
         }
 
         String deleteName = parameters[0], deletePhone = parameters[1];
 
         datastore.delete(new DataRecord(deleteName, deletePhone));
+        System.out.println("Ok! Record " + deleteName + " " + deletePhone + " has been deleted\n");
     }
 
     /**
@@ -192,7 +209,7 @@ public class PhonebookDataBase {
     private static void changeName(Scanner commandScanner, Datastore datastore) {
         String[] parameters = getParameters(commandScanner, 3);
         if (parameters == null) {
-            System.out.println("name, phone or newName not specified, no record has been changed");
+            System.out.println("name, phone or newName not specified, no record has been changed\n");
             return;
         }
 
@@ -200,10 +217,11 @@ public class PhonebookDataBase {
 
         Query<DataRecord> record = getRecordFromDatastore(datastore, name, phone);
         if (record == null) {
-            System.out.print("No record with given name and phone");
+            System.out.println("No record with given name and phone\n");
         } else {
             datastore.findAndModify(record,
                     datastore.createUpdateOperations(DataRecord.class).set("name", newName));
+            System.out.println("Ok! " + name + " has been changed to " + newName + "\n");
         }
     }
 
@@ -213,7 +231,7 @@ public class PhonebookDataBase {
     private static void changePhone(Scanner commandScanner, Datastore datastore) {
         String[] parameters = getParameters(commandScanner, 3);
         if (parameters == null) {
-            System.out.println("name, phone or newPhone was not specified, no record has been changed");
+            System.out.println("name, phone or newPhone was not specified, no record has been changed\n");
             return;
         }
 
@@ -221,10 +239,11 @@ public class PhonebookDataBase {
 
         Query<DataRecord> record = getRecordFromDatastore(datastore, name, phone);
         if (record == null) {
-            System.out.print("No record with given name and phone");
+            System.out.print("No record with given name and phone\n");
         } else {
             datastore.findAndModify(record,
                     datastore.createUpdateOperations(DataRecord.class).set("phone", newPhone));
+            System.out.println("Ok! " + phone + " has been changed to " + newPhone + "\n");
         }
     }
 
@@ -232,10 +251,15 @@ public class PhonebookDataBase {
      *
      */
     private static void printAll(Datastore datastore) {
-        for (var record : datastore.createQuery(DataRecord.class)) {
-            System.out.print(record.getName() + record.getPhone() + ", ");
+        for (var iterator = datastore.createQuery(DataRecord.class).iterator(); iterator.hasNext();) {
+            var record = iterator.next();
+            System.out.print(record.getName() + record.getPhone());
+
+            if (iterator.hasNext()) {
+                System.out.print(", ");
+            }
         }
-        System.out.println();
+        System.out.print("\n\n");
     }
 
     /**
