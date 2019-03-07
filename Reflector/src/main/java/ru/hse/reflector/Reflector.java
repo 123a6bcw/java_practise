344e9074d6f3abc5354b88abbffbcb0f2b06f6ea
123a6bcw <ga.karandash@yandex.ru>
2019-03-07 15:05:50 +0300

    Initial project's structure

---
+++ Reflector/src/main/java/ru/hse/reflector/Reflector.java
@@ -0,0 +1,4 @@
+package ru.hse.reflector;
+
+public class Reflector {
+}
---
+++ Reflector/src/test/java/ru/hse/reflector/ReflectorTest.java
@@ -0,0 +1,7 @@
+package ru.hse.reflector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReflectorTest {
+
+}
