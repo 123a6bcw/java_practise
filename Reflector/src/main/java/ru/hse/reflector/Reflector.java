b2363ee554200b98d8e08ce5bc7511c82da266c1
123a6bcw <ga.karandash@yandex.ru>
2019-03-08 10:59:12 +0300

    initial structure fix

--- Reflector/src/main/java/ru/hse/reflector/Reflector.java
+++ Reflector/src/main/java/ru/hse/reflector/Reflector.java
@@ -1,4 +1,19 @@
 package ru.hse.reflector;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Reflector {
+    public void printStructure(Class<?> someClass) throws IOException {
+        var file = new File("sdeemeedomeClass.java");
+        if (!file.canWrite()) {
+            throw new RuntimeException("Can't read file someClass.java");
+        }
+
+        var writer = new FileWriter(file);
+
+
+    }
 }
