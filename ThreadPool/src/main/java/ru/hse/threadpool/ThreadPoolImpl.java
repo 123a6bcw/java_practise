500a74fe3c2e0a32f65d4601419bcaf413698868
123a6bcw <ga.karandash@yandex.ru>
2019-04-28 19:49:23 +0300

    Moved LightFutureImpl into ThreadPoolImpl

--- ThreadPool/src/main/java/ru/hse/threadpool/ThreadPoolImpl.java
+++ ThreadPool/src/main/java/ru/hse/threadpool/ThreadPoolImpl.java
@@ -1,4 +1,73 @@
 package ru.hse.threadpool;
 
+import java.util.function.Function;
+import java.util.function.Supplier;
+
 public class ThreadPoolImpl {
+    public LightFuture<?> submit (Supplier<?> supplier) {
+        var task = new LightFutureImpl<>(supplier);
+        return task;
+    }
+
+
+    /**
+     *
+     */
+    private class LightFutureImpl<ResultType> implements LightFuture<ResultType> {
+        /**
+         *
+         */
+        private ResultType result;
+
+        /**
+         *
+         */
+        private volatile Supplier<ResultType> supplier;
+
+        /**
+         *
+         */
+        private LightFuture toDoAfter = null;
+
+        private LightFutureImpl(Supplier<ResultType> supplier) {
+            this.supplier = supplier;
+        }
+
+        @Override
+        public boolean isReady() {
+            return result != null;
+        }
+
+        @Override
+        public ResultType get() {
+            if (supplier == null) {
+                return result;
+            }
+
+            synchronized (this) {
+                if (supplier != null) {
+                    result = supplier.get();
+                    supplier = null;
+                }
+            }
+
+            return result;
+        }
+
+        @Override
+        public <TransformType> LightFuture<TransformType> thenApply(Function<? super ResultType, TransformType> applier) {
+            //So there will be no unchecked cast warning. Optimizer will remove this anyway, I hope...
+            var toDoAfter = new LightFutureImpl<TransformType>(() -> applier.apply(get()));
+            this.toDoAfter = toDoAfter;
+            return toDoAfter;
+        }
+
+        /**
+         *
+         */
+        LightFuture getToDoAfter() {
+            return toDoAfter;
+        }
+    }
+
 }
