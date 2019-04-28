a480a9d4bb2b3c4873297068f86428d293d459d8
123a6bcw <ga.karandash@yandex.ru>
2019-04-28 14:54:40 +0300

    Created LightFuture interface

---
+++ ThreadPool/src/main/java/ru/hse/threadpool/LightFuture.java
@@ -0,0 +1,37 @@
+package ru.hse.threadpool;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+/**
+ * Simple interface for evaluating expressions with (probably) long execution time.
+ */
+public interface LightFuture<ResultType> {
+    /** /TODO
+     * Returns true if expression was evaluated.
+     */
+    public boolean isReady();
+
+    /** /TODO
+     * Returns evaluated value.
+     *
+     * If expressions throws exception during execution, this throws LightExecutionException with original exception
+     * as cause.
+     */
+    public ResultType get();
+
+    /** /TODO
+     * Creates new LightFuture expression to evaluate over the result of the original one.
+     *
+     * If original LightFuture object throws and exception during execution, calling get on new object throws
+     * LightExecutionException with original LightExecutionException as cause.
+     */
+    public <TransformType> LightFuture<TransformType> thenApply(Function<ResultType, TransformType> applier);
+
+    /** /TODO
+     * Exception of evaluating LightFuture expressions. If was created as a result of exception during execution of
+     * the expression, stores
+     */
+    public class LightExecutionException extends RuntimeException {
+    }
+}
